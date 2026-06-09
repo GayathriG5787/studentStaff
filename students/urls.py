23a3bd577cf68aa5ad3a4460254f60bcaabf2a49
GayathriG5787 <gayathrigcse2022@gmail.com>
2026-06-09 14:42:28 +0530

    logout button added in both dashboards

--- students/urls.py
+++ students/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import AdminDashboard, StudentDashboard
 
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
     path('admin-dashboard/', AdminDashboard.as_view(), name='admin-dashboard'),
     path('student-dashboard/', StudentDashboard.as_view(), name='student-dashboard'),
+    path('logout/', LogoutView.as_view(next_page = 'login'), name='logout')
 ]
