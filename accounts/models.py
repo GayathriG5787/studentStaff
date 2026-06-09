a4259fcc4cf8088e9ba7efeee0e922417deffb99
GayathriG5787 <gayathrigcse2022@gmail.com>
2026-06-09 12:00:50 +0530

    User model created with role, ran migrations, registered in admin.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    # 'admin' will be stored in DB, while 'Admin' will be showed to the users
+    id = models.UUIDField(
+        primary_key= True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    
+    ROLE_CHOICES = (('admin', 'Admin'), ('student', 'Student'), )
+    role = models.CharField(max_length= 20, choices= ROLE_CHOICES )
