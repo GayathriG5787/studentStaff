232f20d400df96ca8146be17bbb494e15f0c8902
GayathriG5787 <gayathrigcse2022@gmail.com>
2026-06-09 10:09:44 +0530

    university home page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def home(request):
-    return HttpResponse("Hello World")
+# from django.http import HttpResponse
+
+# def home(request):
+#     return HttpResponse("Hello World")
+
+class HomeView(TemplateView):
+    template_name = 'accounts/home.html'
+
