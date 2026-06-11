2ac9bd27526e20f349d7512eae24ca3718e6f263
GayathriG5787 <gayathrigcse2022@gmail.com>
2026-06-11 12:11:51 +0530

    password validation when creating students done

--- students/forms.py
+++ students/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Student
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 class StudentCreateForm(forms.ModelForm):
     username = forms.CharField()
+    # used to hide the content when someone types in the password field
     password = forms.CharField(
         widget=forms.PasswordInput
     )
@@ -15,3 +18,13 @@ class StudentCreateForm(forms.ModelForm):
             'department',
             'year'
         ]
+        
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            raise forms.ValidationError(e.messages)
+        
+        return password
